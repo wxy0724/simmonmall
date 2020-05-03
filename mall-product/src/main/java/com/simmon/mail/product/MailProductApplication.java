392e953d06afa1e5b1c08751760fb5c1b3f48a2c
simmoon <bryan0724@126.com>
2020-05-03 10:25:33 +0800

    项目初始化

---
+++ mall-product/src/main/java/com/simmon/mail/product/MailProductApplication.java
@@ -0,0 +1,13 @@
+package com.simmon.mail.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailProductApplication.class, args);
+	}
+
+}
